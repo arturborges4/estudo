89c490f42c0f72602d9f694da2fa1f4df101b426
arturborges4 <113465290+arturborges4@users.noreply.github.com>
2022-09-13 15:22:34 -0300

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace projetoinicial_mvc.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
